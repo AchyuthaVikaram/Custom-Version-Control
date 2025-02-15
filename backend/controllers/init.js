c816b17fc172cfa31c08e9f17a1b07966617b150
Achyutha Vikaram <achyuthavikaram@gmail.com>
2025-02-15 12:07:49 +0530

    init logic done

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,18 @@
-function init(){
-    console.log("init called");
+const fs = require("fs").promises;//promises is a utility to create a file/folder
+const path = require("path");
+
+async function init() {
+	const repoPath=path.resolve(process.cwd(),".customGit");
+    const commitsPath=path.join(repoPath,"commits");
+    try {
+        
+        await fs.mkdir(repoPath,{recursive:true});
+        await fs.mkdir(commitsPath,{recursive:true});
+        await fs.writeFile(path.join(repoPath,"config.json"),JSON.stringify({bucket:"S3 BUCKET"}));
+        console.log("repository initialized");
+    } catch (error) {
+        console.log("error initializing error :"+error);
+    }
 }
 
-module.exports={init}; 
+module.exports = { init };
